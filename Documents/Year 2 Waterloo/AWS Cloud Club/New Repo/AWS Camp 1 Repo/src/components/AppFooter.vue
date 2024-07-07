04ce9480106a9777cc488795604dcbba54b1bf96
JuliaIlioukhina001 <143233933+JuliaIlioukhina001@users.noreply.github.com>
2024-07-06 20:14:41 -0400

    initial

---
+++ Documents/Year 2 Waterloo/AWS Cloud Club/New Repo/AWS Camp 1 Repo/src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <v-footer
+    color="primary"
+    absolute
+    inset
+    app
+    height="10"
+    width="auto"
+    class="pa-6"
+  >
+    <span>Workshop hosted by the UW AWS Team.</span>
+  </v-footer>
+</template>
